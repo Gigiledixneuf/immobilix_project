f2df43d21e7269de9891cf5f646cb955ddc53136
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-10-05 21:38:38 +0100

    feat: implementation de l'authentification dans LoginController

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,17 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import LoginController from '#controllers/Auth/login_controller'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+// Routes publiques (guest)
+router
+  .group(() => {
+    router.post('login', [LoginController, 'login'])
+  })
+  .prefix('/api')
