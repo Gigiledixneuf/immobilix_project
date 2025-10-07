4d90a12dbe1a41e4d7f848631d8466aad375b34c
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-10-07 21:32:01 +0100

    feat: creation de table properties et mise en relation entre models User et Property

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,11 +1,12 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany, manyToMany } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
 import Role from '#models/role'
-import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+import type { HasMany, ManyToMany } from '@adonisjs/lucid/types/relations'
+import Property from '#models/property'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -38,6 +39,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
     pivotTable: 'user_roles',
   })
   public roles: ManyToMany<typeof Role> | undefined
+
+  @hasMany(() => Property)
+  declare Property: HasMany<typeof Property>
 
   static accessTokens = DbAccessTokensProvider.forModel(User)
 }
