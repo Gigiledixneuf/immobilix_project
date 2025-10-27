bde577757507b52272d003627f3df108b7740aa8
AlbusDigi <albus.digit@gmail.com>
2025-10-27 22:44:39 +0100

    feat(Auth) : ajout du champ token dans le model User, - Ajout de la methode login et register dans le service et implementation

--- lib/business/models/user/user.dart
+++ lib/business/models/user/user.dart
@@ -4,18 +4,21 @@ class User {
   final String? id;
   final String? name;
   final String? email;
+  final String? token; // Ajout du champ token
 
-  User({this.id, this.name, this.email});
+  User({this.id, this.name, this.email, this.token});
 
-  factory User.fromJson(json) => User(
-        id: json['id'],
-        name: json['name'],
-        email: json['email'],
-      );
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    id: json['id']?.toString(), // Correction: Conversion en String
+    name: json['name'],
+    email: json['email'],
+    token: json['token'], // Lecture du token depuis le JSON
+  );
 
-  Map toJson() => {
-        'id': id,
-        'name': name,
-        'email': email,
-      };
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'name': name,
+    'email': email,
+    'token': token, // Ajout du token au JSON
+  };
 }
